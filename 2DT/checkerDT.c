48ee387aec7e9156d791f99e1507be11e03a7e11
Roy Mazumder <rm2033@princeton.edu>
2022-11-12 19:57:06 -0500

    fewf

--- 2DT/checkerDT.c
+++ 2DT/checkerDT.c
@@ -10,17 +10,17 @@
 #include "dynarray.h"
 #include "path.h"
 
-
-
 /* see checkerDT.h for specification */
-boolean CheckerDT_Node_isValid(Node_T oNNode) {
+boolean CheckerDT_Node_isValid(Node_T oNNode)
+{
    Node_T oNParent;
    Path_T oPNPath;
    Path_T oPPPath;
    size_t identifier;
 
    /* Sample check: a NULL pointer is not a valid node */
-   if(oNNode == NULL) {
+   if (oNNode == NULL)
+   {
       fprintf(stderr, "A node is a NULL pointer\n");
       return FALSE;
    }
@@ -28,12 +28,14 @@ boolean CheckerDT_Node_isValid(Node_T oNNode) {
    /* Sample check: parent's path must be the longest possible
       proper prefix of the node's path */
    oNParent = Node_getParent(oNNode);
-   if(oNParent != NULL) {
+   if (oNParent != NULL)
+   {
       oPNPath = Node_getPath(oNNode);
       oPPPath = Node_getPath(oNParent);
 
-      if(Path_getSharedPrefixDepth(oPNPath, oPPPath) !=
-         Path_getDepth(oPNPath) - 1) {
+      if (Path_getSharedPrefixDepth(oPNPath, oPPPath) !=
+          Path_getDepth(oPNPath) - 1)
+      {
          fprintf(stderr, "P-C nodes don't have P-C paths: (%s) (%s)\n",
                  Path_getPathname(oPPPath), Path_getPathname(oPNPath));
          return FALSE;
@@ -42,23 +44,21 @@ boolean CheckerDT_Node_isValid(Node_T oNNode) {
 
    /* Check 1: Node_compare works, i.e. the function applied to two
       referances to the current node return 0 */
-   if(Node_compare(oNNode, oNNode) != 0) {
-      fprintf(stderr, "Node_compare not valid.");
+   if (Node_compare(oNNode, oNNode) != 0)
+   {
+      fprintf(stderr, "Node_compare not valid.\n");
       return FALSE;
    }
 
    /* Check 2: Current Node is identified as its parent's child
       by the Node_hasChild function
-   */ 
-   if (oNParent != NULL && !Node_hasChild(oNParent, oPNPath, &identifier)){
-      fprintf(stderr, "Node_hasChild does not recognize" 
-         "node as a child of its parent node.");  
-         return FALSE;
+   */
+   if (oNParent != NULL && !Node_hasChild(oNParent, oPNPath, &identifier))
+   {
+      fprintf(stderr, "Node_hasChild does not recognize"
+                      " node as a child of its parent node.\n");
+      return FALSE;
    }
-
-
-
-
 
    return TRUE;
 }
@@ -72,30 +72,51 @@ boolean CheckerDT_Node_isValid(Node_T oNNode) {
    parameter list to facilitate constructing your checks.
    If you do, you should update this function comment.
 */
-static boolean CheckerDT_treeCheck(Node_T oNNode) {
-   size_t ulIndex;
+static boolean CheckerDT_treeCheck(Node_T oNNode)
+{
+   size_t ulIndex, ulIndex2;
 
-   if(oNNode!= NULL) {
+   if (oNNode != NULL)
+   {
 
       /* Sample check on each node: node must be valid */
       /* If not, pass that failure back up immediately */
-      if(!CheckerDT_Node_isValid(oNNode))
+      if (!CheckerDT_Node_isValid(oNNode))
          return FALSE;
 
       /* Recur on every child of oNNode */
-      for(ulIndex = 0; ulIndex < Node_getNumChildren(oNNode); ulIndex++)
+      for (ulIndex = 0; ulIndex < Node_getNumChildren(oNNode); ulIndex++)
       {
          Node_T oNChild = NULL;
          int iStatus = Node_getChild(oNNode, ulIndex, &oNChild);
 
-         if(iStatus != SUCCESS) {
+         /* Check 0: Check if number of grandchildren and no. of children returned are same */
+         if (iStatus != SUCCESS)
+         {
             fprintf(stderr, "getNumChildren claims more children than getChild returns\n");
             return FALSE;
          }
 
+         for (ulIndex2 = 0; ulIndex2 < Node_getNumChildren(oNNode); ulIndex2++)
+         {
+            /* Check 1: file paths are unique for nodes in same directory */
+            if (ulIndex == ulIndex2) {
+               continue;
+            }
+            Node_T siblingNode = NULL;
+            int iStatus = Node_getChild(oNNode, ulIndex2, &siblingNode);
+            if (!Path_comparePath(Node_getPath(oNChild), Node_getPath(siblingNode)))
+            {
+               fprintf(stderr, "sibling nodes have duplicate path names\n");
+               return FALSE;
+            }
+         }
+
+         /* Check 2: */
+
          /* if recurring down one subtree results in a failed check
             farther down, passes the failure back up immediately */
-         if(!CheckerDT_treeCheck(oNChild))
+         if (!CheckerDT_treeCheck(oNChild))
             return FALSE;
       }
    }
@@ -104,12 +125,14 @@ static boolean CheckerDT_treeCheck(Node_T oNNode) {
 
 /* see checkerDT.h for specification */
 boolean CheckerDT_isValid(boolean bIsInitialized, Node_T oNRoot,
-                          size_t ulCount) {
+                          size_t ulCount)
+{
 
    /* Sample check on a top-level data structure invariant:
       if the DT is not initialized, its count should be 0. */
-   if(!bIsInitialized)
-      if(ulCount != 0) {
+   if (!bIsInitialized)
+      if (ulCount != 0)
+      {
          fprintf(stderr, "Not initialized, but count is not 0\n");
          return FALSE;
       }
