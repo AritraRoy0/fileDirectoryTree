c637d81d90fc944e85f7e31dfa4fd348e68262d3
Roy Mazumder <aritro.roy0@gmail.com>
2022-11-16 11:31:08 -0500

    ft.c helpers done

--- 3FT/ft.c
+++ 3FT/ft.c
@@ -27,3 +27,137 @@ static Node_T oNRoot;
 /* 3. a counter of the number of nodes in the hierarchy */
 static size_t ulCount;
 
+/* --------------------------------------------------------------------
+
+  The DT_traversePath and DT_findNode functions modularize the common
+  functionality of going as far as possible down an DT towards a path
+  and returning either the node of however far was reached or the
+  node if the full path was reached, respectively.
+*/
+
+/*
+  Traverses the DT starting at the root as far as possible towards
+  absolute path oPPath. If able to traverse, returns an int SUCCESS
+  status and sets *poNFurthest to the furthest node reached (which may
+  be only a prefix of oPPath, or even NULL if the root is NULL).
+  Otherwise, sets *poNFurthest to NULL and returns with status:
+  * CONFLICTING_PATH if the root's path is not a prefix of oPPath
+  * MEMORY_ERROR if memory could not be allocated to complete request
+*/
+static int FT_traversePath(Path_T oPPath, Dir_T *poNFurthest) {
+   int iStatus;
+   Path_T oPPrefix = NULL;
+   Dir_T oNCurr;
+   Dir_T oNChild = NULL;
+   size_t ulDepth;
+   size_t i;
+   size_t ulChildID;
+
+   assert(oPPath != NULL);
+   assert(poNFurthest != NULL);
+
+   /* root is NULL -> won't find anything */
+   if(oNRoot == NULL) {
+      *poNFurthest = NULL;
+      return SUCCESS;
+   }
+
+   iStatus = Path_prefix(oPPath, 1, &oPPrefix);
+   if(iStatus != SUCCESS) {
+      *poNFurthest = NULL;
+      return iStatus;
+   }
+
+   if(Path_comparePath(Dir_getPath(oNRoot), oPPrefix)) {
+      Path_free(oPPrefix);
+      *poNFurthest = NULL;
+      return CONFLICTING_PATH;
+   }
+   Path_free(oPPrefix);
+   oPPrefix = NULL;
+
+   oNCurr = oNRoot;
+   ulDepth = Path_getDepth(oPPath);
+   for(i = 2; i <= ulDepth; i++) {
+      iStatus = Path_prefix(oPPath, i, &oPPrefix);
+      if(iStatus != SUCCESS) {
+         *poNFurthest = NULL;
+         return iStatus;
+      }
+      if(Dir_hasChild(oNCurr, oPPrefix, &ulChildID)) {
+         /* go to that child and continue with next prefix */
+         Path_free(oPPrefix);
+         oPPrefix = NULL;
+         iStatus = Dir_getSubDir(oNCurr, ulChildID, &oNChild);
+         if(iStatus != SUCCESS) {
+            *poNFurthest = NULL;
+            return iStatus;
+         }
+         oNCurr = oNChild;
+      }
+      else {
+         /* oNCurr doesn't have child with path oPPrefix:
+            this is as far as we can go */
+         break;
+      }
+   }
+
+   Path_free(oPPrefix);
+   *poNFurthest = oNCurr;
+   return SUCCESS;
+}
+
+/*
+  Traverses the DT to find a node with absolute path pcPath. Returns a
+  int SUCCESS status and sets *poNResult to be the node, if found.
+  Otherwise, sets *poNResult to NULL and returns with status:
+  * INITIALIZATION_ERROR if the DT is not in an initialized state
+  * BAD_PATH if pcPath does not represent a well-formatted path
+  * CONFLICTING_PATH if the root's path is not a prefix of pcPath
+  * NO_SUCH_PATH if no node with pcPath exists in the hierarchy
+  * MEMORY_ERROR if memory could not be allocated to complete request
+ */
+static int FT_findDir(const char *pcPath, Dir_T *poNResult) {
+   Path_T oPPath = NULL;
+   Dir_T oNFound = NULL;
+   int iStatus;
+
+   assert(pcPath != NULL);
+   assert(poNResult != NULL);
+
+   if(!bIsInitialized) {
+      *poNResult = NULL;
+      return INITIALIZATION_ERROR;
+   }
+
+   iStatus = Path_new(pcPath, &oPPath);
+   if(iStatus != SUCCESS) {
+      *poNResult = NULL;
+      return iStatus;
+   }
+
+   iStatus = FT_traversePath(oPPath, &oNFound);
+   if(iStatus != SUCCESS)
+   {
+      Path_free(oPPath);
+      *poNResult = NULL;
+      return iStatus;
+   }
+
+   if(oNFound == NULL) {
+      Path_free(oPPath);
+      *poNResult = NULL;
+      return NO_SUCH_PATH;
+   }
+
+   if(Path_comparePath(Dir_getPath(oNFound), oPPath) != 0) {
+      Path_free(oPPath);
+      *poNResult = NULL;
+      return NO_SUCH_PATH;
+   }
+
+   Path_free(oPPath);
+   *poNResult = oNFound;
+   return SUCCESS;
+}
+/*--------------------------------------------------------------------*/
