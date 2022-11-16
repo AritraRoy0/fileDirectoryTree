ac7bcf17f10b77332426260842c3a3fef5df7523
Roshaan Khalid <roshaan1921@gmail.com>
2022-11-16 13:00:45 -0500

    yo

--- 3FT/ft.c
+++ 3FT/ft.c
@@ -27,3 +27,105 @@ static Node_T oNRoot;
 /* 3. a counter of the number of nodes in the hierarchy */
 static size_t ulCount;
 
+
+/*
+  Sets the FT data structure to an initialized state.
+  The data structure is initially empty.
+  Returns INITIALIZATION_ERROR if already initialized,
+  and SUCCESS otherwise.
+*/
+int FT_init(void){
+  assert(CheckerDT_isValid(bIsInitialized, oNRoot, ulCount));
+
+   if(bIsInitialized)
+      return INITIALIZATION_ERROR;
+
+   bIsInitialized = TRUE;
+   oNRoot = NULL;
+   ulCount = 0;
+
+   assert(CheckerDT_isValid(bIsInitialized, oNRoot, ulCount));
+   return SUCCESS;
+}
+
+/*
+  Removes all contents of the data structure and
+  returns it to an uninitialized state.
+  Returns INITIALIZATION_ERROR if not already initialized,
+  and SUCCESS otherwise.
+*/
+int FT_destroy(void){
+  assert(CheckerDT_isValid(bIsInitialized, oNRoot, ulCount));
+
+   if(!bIsInitialized)
+      return INITIALIZATION_ERROR;
+
+   if(oNRoot) {
+      ulCount -= Node_free(oNRoot);
+      oNRoot = NULL;
+   }
+
+   bIsInitialized = FALSE;
+
+   assert(CheckerDT_isValid(bIsInitialized, oNRoot, ulCount));
+   return SUCCESS;
+}
+
+/*
+  Returns TRUE if the FT contains a directory with absolute path
+  pcPath and FALSE if not or if there is an error while checking.
+*/
+boolean FT_containsDir(const char *pcPath){
+  int iStatus;
+   Node_T oNFound = NULL;
+
+   assert(pcPath != NULL);
+
+   iStatus = FT_findNode(pcPath, &oNFound);
+   return (boolean) (iStatus == SUCCESS);
+}
+
+/*
+   Inserts a new directory into the FT with absolute path pcPath.
+   Returns SUCCESS if the new directory is inserted successfully.
+   Otherwise, returns:
+   * INITIALIZATION_ERROR if the FT is not in an initialized state
+   * BAD_PATH if pcPath does not represent a well-formatted path
+   * CONFLICTING_PATH if the root exists but is not a prefix of pcPath
+   * NOT_A_DIRECTORY if a proper prefix of pcPath exists as a file
+   * ALREADY_IN_TREE if pcPath is already in the FT (as dir or file)
+   * MEMORY_ERROR if memory could not be allocated to complete request
+*/
+int FT_insertDir(const char *pcPath);
+
+/*
+  Removes the FT hierarchy (subtree) at the directory with absolute
+  path pcPath. Returns SUCCESS if found and removed.
+  Otherwise, returns:
+  * INITIALIZATION_ERROR if the FT is not in an initialized state
+  * BAD_PATH if pcPath does not represent a well-formatted path
+  * CONFLICTING_PATH if the root exists but is not a prefix of pcPath
+  * NO_SUCH_PATH if absolute path pcPath does not exist in the FT
+  * NOT_A_DIRECTORY if pcPath is in the FT as a file not a directory
+  * MEMORY_ERROR if memory could not be allocated to complete request
+*/
+int FT_rmDir(const char *pcPath){
+  int iStatus;
+   File_T oNFound = NULL;
+
+   assert(pcPath != NULL);
+   assert(CheckerDT_isValid(bIsInitialized, oNRoot, ulCount));
+
+   iStatus = DT_findNode(pcPath, &oNFound);
+
+   if(iStatus != SUCCESS)
+       return iStatus;
+
+   ulCount -= Node_free(oNFound);
+   if(ulCount == 0)
+      oNRoot = NULL;
+
+   assert(CheckerDT_isValid(bIsInitialized, oNRoot, ulCount));
+   return SUCCESS;
+}
+
