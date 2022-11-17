d11c3265ef008140cc42d4df71678a4f7a9682e7
Roy Mazumder <aritro.roy0@gmail.com>
2022-11-17 14:47:45 -0500

    asdas

--- 3FT/ft_client.c
+++ 3FT/ft_client.c
@@ -83,10 +83,10 @@ int main(void)
    assert(FT_containsDir("1root/2child") == TRUE);
    assert(FT_containsFile("1root/2child") == FALSE);
    assert(FT_containsDir("1root/2child/3gkid") == TRUE);
+
+   assert(FT_containsFile("1root/2child/3gkid") == FALSE);
       /*     ---------------------     */
    assert(FT_containsDir("1root/2child/3gkid") == TRUE);
-   assert(FT_containsFile("1root/2child/3gkid") == FALSE);
-
    assert(FT_insertFile("1root/2second/3gfile", NULL, 0) == SUCCESS);
 
 
