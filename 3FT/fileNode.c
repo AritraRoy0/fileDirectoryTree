616631b170584b4108ad6d1db206102f5eca9d89
Roy Mazumder <aritro.roy0@gmail.com>
2022-11-17 02:10:35 -0500

    first round of bug fixes

--- 3FT/fileNode.c
+++ 3FT/fileNode.c
@@ -65,7 +65,7 @@ int File_new(Path_T oPPath, Dir_T oNParent, File_T *poNResult)
    assert(oNParent == NULL);
 
    /* allocate space for a new node */
-   psNew = malloc(sizeof(struct fileNode));
+   psNew = (fileNode *)calloc(sizeof(struct fileNode));
    if (psNew == NULL)
    {
       *poNResult = NULL;
